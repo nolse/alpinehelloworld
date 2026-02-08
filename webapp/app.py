6951c9aee92815c9ce5c6e9a63a610d53723107e
nolse <91975392+nolse@users.noreply.github.com>
2026-02-08 10:43:28 +0100

    Fix hello function response and clean up code
    Signed-off-by: nolse <91975392+nolse@users.noreply.github.com>

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,7 @@
-import os
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello world!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    return "Hello World!"
